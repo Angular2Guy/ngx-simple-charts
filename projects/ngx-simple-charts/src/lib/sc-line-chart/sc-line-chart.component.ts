571700e3ac2c49dcb54408d937792c42874f7162
Angular2Guy <angular2guy@gmail.com>
2021-09-04 19:31:14 +0200

    first step

--- projects/ngx-simple-charts/src/lib/sc-line-chart/sc-line-chart.component.ts
+++ projects/ngx-simple-charts/src/lib/sc-line-chart/sc-line-chart.component.ts
@@ -3,18 +3,18 @@
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-       http://www.apache.org/licenses/LICENSE-2.0
+	   http://www.apache.org/licenses/LICENSE-2.0
    Unless required by applicable law or agreed to in writing, software
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-import { Component, ViewChild, ElementRef, Input, OnChanges, SimpleChanges, ViewEncapsulation, AfterViewInit, HostListener, OnDestroy } from '@angular/core';
+import { Component, ViewChild, ElementRef, Input, OnChanges, SimpleChanges, ViewEncapsulation, AfterViewInit, HostListener, OnDestroy, Output } from '@angular/core';
 import { select, Selection, ContainerElement } from 'd3-selection';
 import { scaleLinear, scaleTime, ScaleLinear, ScaleTime } from 'd3-scale';
 import { extent } from 'd3-array';
-import { line, curveMonotoneX } from 'd3-shape';
+import { line, curveMonotoneX, Line } from 'd3-shape';
 import { axisBottom, axisLeft } from 'd3-axis';
 import { ChartPoints, ChartPoint } from '../model/chart-points';
 import { Subject, Subscription } from 'rxjs';
@@ -24,14 +24,25 @@ interface ResizeEvent {
 	type: string;
 }
 
+interface MultiLineSeries {
+	name: string;
+	values: number[];
+}
+
+interface MultiLineData {
+	y: string;
+	series: MultiLineSeries[];
+	xValues: string | Date;
+}
+
 @Component({
 	selector: 'sc-line-chart',
 	templateUrl: './sc-line-chart.component.html',
 	styleUrls: ['./sc-line-chart.component.scss'],
 	encapsulation: ViewEncapsulation.Emulated,
 })
-export class ScLineChartComponent implements AfterViewInit, OnChanges, OnDestroy {    
-	@ViewChild("svgchart", {static: true})
+export class ScLineChartComponent implements AfterViewInit, OnChanges, OnDestroy {
+	@ViewChild("svgchart", { static: true })
 	private chartContainer!: ElementRef;
 	d3Svg!: Selection<ContainerElement, ChartPoint, HTMLElement, any>;
 	@Input()
@@ -40,7 +51,6 @@ export class ScLineChartComponent implements AfterViewInit, OnChanges, OnDestroy
 	private chartUpdateSubscription!: Subscription;
 	private gxAttribute!: Selection<SVGGElement, ChartPoint, HTMLElement, any>;
 	private gyAttribute!: Selection<SVGGElement, ChartPoint, HTMLElement, any>;
-	private gPathAttribute!: Selection<SVGPathElement, ChartPoint, HTMLElement, any>;
 
 	ngAfterViewInit(): void {
 		this.d3Svg = select<ContainerElement, ChartPoint>(this.chartContainer.nativeElement);
@@ -49,22 +59,21 @@ export class ScLineChartComponent implements AfterViewInit, OnChanges, OnDestroy
 		this.gxAttribute = this.gxAttribute.attr('class', 'x axis');
 		this.gyAttribute = this.d3Svg.append('g');
 		this.gyAttribute = this.gyAttribute.attr('class', 'y axis');
-		this.gPathAttribute = this.d3Svg.append('path');
-		this.chartUpdateSubscription = this.chartUpdateSubject.pipe(debounceTime(100)).subscribe(() => this.updateChart());	
-		this.chartUpdateSubject.next({});	
+		this.chartUpdateSubscription = this.chartUpdateSubject.pipe(debounceTime(100)).subscribe(() => this.updateChart());
+		this.chartUpdateSubject.next({});
 	}
 
 	ngOnDestroy(): void {
 		this.chartUpdateSubject.complete();
-        this.chartUpdateSubscription.unsubscribe();
-    }
+		this.chartUpdateSubscription.unsubscribe();
+	}
 
 	ngOnChanges(changes: SimpleChanges): void {
 		if (!!changes['chartPoints'] && !changes['chartPoints'].isFirstChange()) {
 			this.chartUpdateSubject.next({});
 		}
 	}
-	
+
 	@HostListener('window:resize', ['$event'])
 	onResize(event: ResizeEvent): void {
 		this.chartUpdateSubject.next({});
@@ -72,22 +81,82 @@ export class ScLineChartComponent implements AfterViewInit, OnChanges, OnDestroy
 
 	private updateChart(): void {
 		const contentWidth = isNaN(parseInt(this.d3Svg.style('width')
-			.replace(/[^0-9\.]+/g, ''), 10)) ? 
+			.replace(/[^0-9\.]+/g, ''), 10)) ?
 			0 : parseInt(this.d3Svg.style('width')
-			.replace(/[^0-9\.]+/g, ''), 10);
+				.replace(/[^0-9\.]+/g, ''), 10);
 		const contentHeight = isNaN(parseInt(this.d3Svg.style('height')
-			.replace(/[^0-9\.]+/g, ''), 10)) ? 
+			.replace(/[^0-9\.]+/g, ''), 10)) ?
 			0 : parseInt(this.d3Svg.style('height')
-			.replace(/[^0-9\.]+/g, ''), 10);
+				.replace(/[^0-9\.]+/g, ''), 10);
 		this.d3Svg.attr("viewBox", [0, 0, contentWidth, contentHeight] as any)
-		if (contentHeight < 1 || contentWidth < 1 || !this.chartPoints 
-			|| this.chartPoints.length === 0 || !this.chartPoints[0].chartPointList 
+		if (contentHeight < 1 || contentWidth < 1 || !this.chartPoints
+			|| this.chartPoints.length === 0 || !this.chartPoints[0].chartPointList
 			|| this.chartPoints[0].chartPointList.length === 0) {
 			console.log(`contentHeight: ${contentHeight} contentWidth: ${contentWidth} chartPoints: ${this.chartPoints.length}`);
 			return;
 		}
 		//console.log(`chartPoints: ${this.chartPoints.length} chartPointList: ${this.chartPoints[0].chartPointList.length}`);
+
+		if (this.chartPoints.length <= 1) {
+			this.updateSingleLine(contentHeight, contentWidth);
+		} else {
+			this.updateMultiLine(contentHeight, contentWidth);
+		}
+	}
+
+	private updateMultiLine(contentHeight: number, contentWidth: number) {
+		const xScaleValues = new Set(...this.chartPoints.map(myChartPoint => myChartPoint.chartPointList.map(myChartPointElement => myChartPointElement.x)));
+		let xScale: ScaleTime<number, number, never> | ScaleLinear<number, number, never>;
+		if (this.chartPoints[0].chartPointList[0].x instanceof Date) {
+			xScale = scaleTime()
+				.domain(extent(Array.from(xScaleValues), p => p as Date) as [Date, Date])
+				.range([0, contentWidth - this.chartPoints[0].yScaleWidth])
+		} else {
+			xScale = scaleLinear()
+				.domain([0, this.chartPoints[0].chartPointList.length - 1]).nice()
+				.range([0, contentWidth - this.chartPoints[0].yScaleWidth]);
+		}
+
+		//console.log(xScale);
+		const yScaleValues = [].concat.apply(() => this.chartPoints.map(myChartPoints => myChartPoints.chartPointList)) as ChartPoint[];
+		const yScale = scaleLinear()
+			.domain(extent<ChartPoint, number>(yScaleValues, p => p.y) as [number, number]).nice()
+			.range([contentHeight - this.chartPoints[0].xScaleHeight, 0]);
+
+		const myLine = this.updateScalesLine(contentHeight, xScale, yScale);
+
+		/*this.d3Svg.append('g')
+			.selectAll('path')
+			.datum(this.chartPoints.map(myChartPoints => {name: myChartPoints.name, values: myChartPoints.chartPointList.map(myChartPoint => myChartPoint.y)}))
+			.join('path')
+			.style("mix-blend-mode", "multiply")
+			.attr("d", d => line(d.values));*/
+			
 		
+	}
+
+	private updateScalesLine(contentHeight: number,  
+		xScale: ScaleTime<number, number, never> | ScaleLinear<number, number, never>, 
+		yScale: ScaleLinear<number, number, never>): Line<[number, number]> {
+		this.gxAttribute
+			.attr("transform", "translate(" + (this.chartPoints[0].yScaleWidth) + ","
+				+ (contentHeight - this.chartPoints[0].xScaleHeight) + ")")
+			.call(axisBottom(xScale));
+
+		this.gyAttribute
+			.attr("transform", "translate(" + (this.chartPoints[0].yScaleWidth) + "," + 0 + ")")
+			.call(axisLeft(yScale));
+			
+		const myLine = line()
+			.defined(p => (p as unknown as ChartPoint).y !== null && !isNaN((p as unknown as ChartPoint).y))
+			.x((p, i) => xScale((p as unknown as ChartPoint).x instanceof Date ?
+				(p as unknown as ChartPoint).x as Date : i))
+			.y((p) => yScale((p as unknown as ChartPoint).y))
+			.curve((p) => curveMonotoneX(p));
+		return myLine;
+	}
+
+	private updateSingleLine(contentHeight: number, contentWidth: number) {
 		let xScale: ScaleTime<number, number, never> | ScaleLinear<number, number, never>;
 		if (this.chartPoints[0].chartPointList[0].x instanceof Date) {
 			xScale = scaleTime()
@@ -105,25 +174,10 @@ export class ScLineChartComponent implements AfterViewInit, OnChanges, OnDestroy
 			.domain(extent<ChartPoint, number>(this.chartPoints[0].chartPointList, p => p.y) as [number, number]).nice()
 			.range([contentHeight - this.chartPoints[0].xScaleHeight, 0]);
 
-		const myLine = line()
-			.defined(p => (p as unknown as ChartPoint).y !== null && !isNaN((p as unknown as ChartPoint).y))
-			.x((p,i) => xScale((p as unknown as ChartPoint).x instanceof Date ? 
-				(p as unknown as ChartPoint).x as Date: i))
-			.y((p) => yScale((p as unknown as ChartPoint).y))
-			.curve((p) => curveMonotoneX(p));
-		
-		
-		this.gPathAttribute.datum(this.chartPoints[0].chartPointList)
-			.attr('transform', 'translate('+ this.chartPoints[0].yScaleWidth +', 0)')
-			.attr('class', 'line').attr('d', myLine as any);
+		const myLine = this.updateScalesLine(contentHeight, xScale, yScale);
 
-		this.gxAttribute
-			.attr("transform", "translate(" + (this.chartPoints[0].yScaleWidth) + "," 
-				+ (contentHeight - this.chartPoints[0].xScaleHeight) + ")")
-			.call(axisBottom(xScale));
-
-		this.gyAttribute
-			.attr("transform", "translate(" + (this.chartPoints[0].yScaleWidth) + "," + 0 + ")")
-			.call(axisLeft(yScale));		
+		this.d3Svg.append('path').datum(this.chartPoints[0].chartPointList)
+			.attr('transform', 'translate(' + this.chartPoints[0].yScaleWidth + ', 0)')
+			.attr('class', 'line').attr('d', myLine as any);				
 	}
 }

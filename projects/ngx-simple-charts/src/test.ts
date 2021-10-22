34a1276bb99f654be0dddcb11550d240f08b63a0
Angular2Guy <angular2guy@gmail.com>
2021-10-22 21:23:51 +0200

    feat: new entry points

--- projects/ngx-simple-charts/src/test.ts
+++ projects/ngx-simple-charts/src/test.ts
@@ -35,6 +35,6 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context('../', true, /src\/lib\/.+\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);

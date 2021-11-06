16f4b4fba03f53b7ea5464049893b510bf5a5a60
Angular2Guy <angular2guy@gmail.com>
2021-11-06 15:10:15 +0100

    feat: bar charts first shot

--- projects/ngx-simple-charts/bar/src/lib/sc-bar-chart/sc-bar-chart.component.ts
+++ projects/ngx-simple-charts/bar/src/lib/sc-bar-chart/sc-bar-chart.component.ts
@@ -19,12 +19,19 @@ import {
   AfterViewInit,
   Component,
   SimpleChanges,
+  HostListener,
 } from '@angular/core';
-import { extent } from 'd3-array';
+import { extent, max, min } from 'd3-array';
 import { axisBottom, axisLeft } from 'd3-axis';
 import { scaleBand, scaleLinear } from 'd3-scale';
-import { ContainerElement, select, Selection } from 'd3-selection';
+import { ContainerElement, select, selectAll, Selection } from 'd3-selection';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { ChartBar, ChartBars } from './model/chart-bars';
+
+interface ResizeEvent {
+  type: string;
+}
 
 @Component({
   selector: 'sc-bar-chart',
@@ -37,16 +44,20 @@ export class ScBarChartComponent
   @ViewChild('svgchart', { static: true })
   private chartContainer!: ElementRef;
   d3Svg!: Selection<ContainerElement, ChartBar, HTMLElement, any>;
-  private gAttribute!: Selection<SVGGElement, ChartBar, HTMLElement, any>;
+  private chartUpdateSubject = new Subject();
+  private chartUpdateSubscription!: Subscription;
   @Input()
   chartBars!: ChartBars;
 
   ngOnDestroy(): void {
-    console.log('destroy');
+    this.chartUpdateSubject.complete();
+    this.chartUpdateSubscription.unsubscribe();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('change');
+    if (!!changes['chartBars'] && !changes['chartBars'].firstChange) {
+      this.chartUpdateSubject.next({});
+    }
   }
 
   ngAfterViewInit(): void {
@@ -54,9 +65,15 @@ export class ScBarChartComponent
       this.chartContainer.nativeElement
     );
 
-    this.gAttribute = this.d3Svg.append('g');
-    this.gAttribute = this.gAttribute.attr('class', 'x axis');
-    this.gAttribute = this.gAttribute.attr('class', 'y axis');
+    this.chartUpdateSubscription = this.chartUpdateSubject
+      .pipe(debounceTime(100))
+      .subscribe(() => this.updateChart());
+    this.chartUpdateSubject.next({});
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: ResizeEvent): void {
+    event && this.chartUpdateSubject.next({});
   }
 
   private updateChart(): void {
@@ -70,50 +87,77 @@ export class ScBarChartComponent
     )
       ? 0
       : parseInt(this.d3Svg.style('height').replace(/[^0-9\.]+/g, ''), 10);
+
     this.d3Svg.attr('viewBox', [0, 0, contentWidth, contentHeight] as any);
+
+    this.d3Svg.selectAll('g').remove();
+    const gxAttributeTemp = this.d3Svg.append('g');
+    const gxAttribute = gxAttributeTemp.attr('class', 'x axis');
+    const gyAttributeTemp = this.d3Svg.append('g');
+    const gyAttribute = gyAttributeTemp.attr('class', 'y axis');
+
     // X axis
-    const x = scaleBand()
-      .range([0, this.chartBars.chartBars.length - 1])
-      .domain(
-        extent(this.chartBars.chartBars, (cb) => cb.x as string) as [
-          string,
-          string
-        ]
-      )
+    const xScale = scaleBand()
+      .domain(this.chartBars.chartBars.map((myBar) => myBar.x))
+      .rangeRound([contentWidth - this.chartBars.yScaleWidth, 0])
       .padding(0.2);
-    this.gAttribute
-      .attr('transform', `translate(0, ${this.chartBars.yScaleWidth})`)
-      .call(axisBottom(x))
+
+    gxAttribute
+      .attr(
+        'transform',
+        'translate(' +
+          this.chartBars.yScaleWidth +
+          ',' +
+          (contentHeight - this.chartBars.xScaleHeight) +
+          ')'
+      )
+      .call(axisBottom(xScale))
       .selectAll('text')
       .attr('transform', 'translate(-10,0)rotate(-45)')
       .style('text-anchor', 'end');
-    // Add Y axis
 
-    const y = scaleLinear()
+    // Add Y axis
+    const yScale = scaleLinear()
       .domain(
-        extent<ChartBar, number>(this.chartBars.chartBars, (p) => p.y) as [
+        [
+          min([0, ...this.chartBars.chartBars.map((myBar) => myBar.y)]),
+          max(this.chartBars.chartBars.map((myBar) => myBar.y)),
+        ] as number[]
+        /*extent<ChartBar, number>(this.chartBars.chartBars, (p) => p.y) as [
           number,
           number
-        ]
+        ]*/
       )
       .nice()
       .range([contentHeight - this.chartBars.xScaleHeight, 0]);
-    this.gAttribute.call(axisLeft(y));
+    gyAttribute
+      .attr(
+        'transform',
+        'translate(' + this.chartBars.yScaleWidth + ',' + 0 + ')'
+      )
+      .call(axisLeft(yScale));
+
+    //console.log(this.gAttribute);
 
     // Bars
-    this.d3Svg.selectAll('path').remove();
+    this.d3Svg.selectAll('#my-chart').remove();
     this.d3Svg
-      .append('path')
+      .append('g')
+      .attr('my-chart', 'my-chart')
+      .selectAll('#my-chart')
       .data(this.chartBars.chartBars)
       .attr('transform', 'translate(' + this.chartBars.yScaleWidth + ', 0)')
       .join('rect')
-      .attr('x', (d) => x(d.x) as unknown as string)
-      .attr('y', (d) => y(d.y))
-      .attr('width', x.bandwidth())
+      .attr(
+        'x',
+        (d) => (xScale(d.x) as unknown as number) + this.chartBars.yScaleWidth
+      )
+      .attr('y', (d) => yScale(d.y))
+      .attr('width', xScale.bandwidth())
       .attr(
         'height',
-        (d) => contentHeight - this.chartBars.xScaleHeight - y(d.y)
+        (d) => contentHeight - this.chartBars.xScaleHeight - yScale(d.y)
       )
-      .attr('fill', '#69b3a2');
+      .attr('fill', '#0000ff');
   }
 }
